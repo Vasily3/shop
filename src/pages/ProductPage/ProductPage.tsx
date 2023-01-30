d68e96962cb64bf59ae42cbce520b468c414440b
Vasily3 <vasilybarkov@mail.ru>
2023-01-30 22:46:42 +0300

    add changes ProductPage

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -15,7 +15,7 @@ const ProductPage: FC = () => {
     const navigate = useNavigate()
 
     function handleClick() {
-        navigate('/');
+        navigate('/')
     }
 
     useEffect(() => {
@@ -32,36 +32,50 @@ const ProductPage: FC = () => {
             {isLoading && <p>Загрузка...</p>}
             {error && <p>Ошибка: {error}</p>}
             <div className='product-page__container container'>
-                <button className='product-page__btn-back' type="button" onClick={handleClick}>Вернуться на главную</button>
+                <button className='product-page__btn-back' type='button' onClick={handleClick}>Вернуться на главную</button>
                 <div className='product-page__header'>
                     <div className='product-page__discount'>
-                        <img className='product-page__discount-img' src={discount} alt="discount"/>
-                        <span className='product-page__discount-value'>-{product?.discount}%</span>
+                        {product?.discount !== '0' &&
+                            <>
+                                <img className='product-page__discount-img' src={discount} alt='discount'/>
+                                <span className='product-page__discount-value'>-{product?.discount}%</span>
+                            </>
+                        }
                     </div>
-                    <img className='product-page__logo' src={product?.logo_url} alt="discount"/>
+                    {product?.logo_url &&
+                        <img className='product-page__logo' src={product?.logo_url} alt='discount'/>
+                    }
                 </div>
 
                 <div className='product-page__info'>
-                    <img className='product-page__img' src={product?.image_url} alt="product"/>
+                    {product?.image_url &&
+                        <img className='product-page__img' src={product?.image_url} alt='product'/>
+                    }
                     <div className='product-page__text-block'>
-                        <h1 className='product-page__title'>{product?.name}</h1>
+                        {product?.name &&
+                            <h1 className='product-page__title'>{product?.name}</h1>
+                        }
                         <ProductRating className='product-page__rating' value={product?.stars}/>
-                        <div className='product-page__prices'>
-                            <div className='product-page__price'>
-                                <span className='product-page__old-price'>{product?.old_price} ₽<span/></span>
-                                <p className='product-page__old-price-text'>старая цена</p>
+                        {product?.new_price ?
+                            <div className='product-page__prices'>
+                                <div className='product-page__price'>
+                                    <span className='product-page__old-price'>{product?.old_price} ₽<span/></span>
+                                    <p className='product-page__old-price-text'>старая цена</p>
+                                </div>
+                                <div className='product-page__price'>
+                                    <span className='product-page__new-price'>{product?.new_price} ₽</span>
+                                    <p className='product-page__new-price-text'>цена по акции</p>
+                                </div>
                             </div>
-                            <div className='product-page__price'>
-                                <span className='product-page__new-price'>{product?.new_price} ₽</span>
-                                <p className='product-page__new-price-text'>цена по акции</p>
-                            </div>
-                        </div>
+                            :
+                            <p className='product-page__new-price-text'>товара нет в наличии</p>
+                        }
                     </div>
                 </div>
             </div>
 
             <div className='product-page__disclaimer'>
-                {product?.disclaimer}
+                {product?.disclaimer? product?.disclaimer : ''}
             </div>
         </article>
     );
